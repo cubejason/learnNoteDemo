a7e97e2f3821bf9c747f0b1947b147be4acc692d
cubejason <cube.jason.lin@gmail.com>
2016-08-06 14:19:13 +0800

    框架
    能被运行的框架

---
+++ goldenDust/goldenDust/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  goldenDust
+//
+//  Created by mac-mini2 on 16/8/6.
+//  Copyright © 2016年 jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ goldenDust/goldenDustTests/goldenDustTests.m
@@ -0,0 +1,39 @@
+//
+//  goldenDustTests.m
+//  goldenDustTests
+//
+//  Created by mac-mini2 on 16/8/6.
+//  Copyright © 2016年 jason. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface goldenDustTests : XCTestCase
+
+@end
+
+@implementation goldenDustTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
